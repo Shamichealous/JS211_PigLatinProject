234b5bb72cd5ce18b9e30d4baf2fe9fe7b2efdbb
shamichealous <ishamichealous@gmail.com>
2022-10-17 21:30:19 -0500

    pig latin game finished

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 const assert = require('assert');
 // brings in the readline module to access the command line
 const readline = require('readline');
+const { getSystemErrorMap } = require('util');
 // use the readline module to print out to the command line
 const rl = readline.createInterface({
   input: process.stdin,
@@ -11,11 +12,88 @@ const rl = readline.createInterface({
 });
 
 
+
+
 const pigLatin = (word) => {
-
   // Your code here
+  //trim word and lowercase before translation//
 
+        
+let trimmedWord = word.trim();
+let vowels = ['a', 'e' , 'i' , 'o' ,'u'] 
+
+if(vowels.indexOf(trimmedWord[0]) > -1)
+{ 
+  trimmedWord = word + "yay";
+  return trimmedWord;
+} else {
+  
+  let firstMatch = word.match(/[aeious]/g)  || 0;
+  // find all of the first consonants and move em to end//
+    let vowel = trimmedWord.indexOf(firstMatch[0]);
+    let firstConsonant = trimmedWord.substring(0, vowel)
+    let leftOverWord = trimmedWord.substring(1, trimmedWord.length)
+    let finalOutput = leftOverWord + firstConsonant + "ay";
+      return finalOutput;
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  //assuming the word is trimmed and lowercased here//
+  //loop through word//
+  
+  // for (let i = 0; i <= word.length; i++)
+  // {
+  //   if 
+  // console.log(word[i])
+    
+  // }
+
+
+  //loop through words that starts with consonant until vowel//
+  //take the consonants from rest of word//
+  //add to the back of the changed word//
+  //exit out of loop when we hit a vowel//
+  //add ay to the end of changed word// 
+//   firstConsonants = firstConsonants + word[0]
+//   console.log("first consonant = " + firstConsonants)
+//   //add capability for dealing with complex consonant words//
+
+//   word = word.slice(1)
+//   console.log("slice =" + word)
+//   word = word + firstConsonants
+//   console.log("slice with consonant =" + word)
+
+//   word = word + "ay"
+//   console.log("final word" + word)
+
+//   return word;
+// }
+// else {
+
+//   (vowels.includes(word))
+// {
+//   word = word +"yay"
+  
+//   return word;
+
+// }
+
+// }
+
+
+  
+
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
